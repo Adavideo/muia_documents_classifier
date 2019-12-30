2ece998fd6b851b3022bb0a9e25f7b650a0b58f6
adavideo <adavideo@gmail.com>
2019-12-30 13:24:46 +0100

    Shows the performance by categories

--- classifier.py
+++ classifier.py
@@ -33,26 +33,66 @@ def svm_classifier():
 def train_classifier(classifier, training_data):
     return classifier.fit(training_data.data, training_data.target)
 
-# Predicts the average performance of the classifier
-def calculate_prediction(classifier, test_data):
-    predicted = classifier.predict(test_data.data)
-    mean_prediction = np.mean(predicted == test_data.target)
-    return mean_prediction
-
-def build_and_test_classifier(classifier_type, training_data, test_data):
-    print("\nBuilding and testing a " + classifier_type + " classifier")
+def build_classifier(classifier_type, training_data):
+    print("Building " + classifier_type + " classifier")
     if classifier_type == "Naive Bayes":
         classifier = naive_bayes_classifier()
     elif classifier_type == "SVM":
         classifier = svm_classifier()
     classifier = train_classifier(classifier,training_data)
-    prediction = calculate_prediction(classifier, test_data)
-    print("Performance prediction: " + str(prediction))
     return classifier
 
+# Compare the predictions with the right categories and returns
+# if the prediction is correct or not, ordered by category
+def calculate_results_by_category(predictions, target_categories):
+    results = { 0:[], 1:[], 2:[] }
+    counter = 0
+    for target in target_categories:
+        results[target].append(target == predictions[counter])
+        counter += 1
+    return results
 
+# Compare the prediction with the real categories to calculate the performance
+def calculate_performance_by_category(predictions, target_categories):
+    results = calculate_results_by_category(predictions, target_categories)
+    performance_by_category = { 0:[], 1:[], 2:[] }
+    for category in results:
+        performance_by_category[category] = np.mean(results[category])
+    return performance_by_category
+
+# Test the classifier with the test data and calculates the performance
+def test_classifier(classifier, test_data):
+    performance = {}
+    predictions = classifier.predict(test_data.data)
+    # Calculate the average performance of the classifier with the test data
+    performance["mean"] = np.mean(predictions == test_data.target)
+    # Calculate the performance of the classifier with the test data by category
+    performance["by_category"] = calculate_performance_by_category(predictions, test_data.target)
+    return performance
+
+def show_results(performance):
+    mean = str(performance["mean"])
+    print("Mean performance: " + mean)
+    for category in performance["by_category"]:
+        print("Category: " + str(category))
+        print(performance["by_category"][category])
+
+
+# Load the datasets
 training_data = load_dataset("train")
 test_data = load_dataset("test")
 
-nb_classifier = build_and_test_classifier("Naive Bayes",  training_data, test_data)
-svm_classifier = build_and_test_classifier("SVM",  training_data, test_data)
+# Build and train the classifiers
+nb_classifier = build_classifier("Naive Bayes",  training_data)
+svm_classifier = build_classifier("SVM",  training_data)
+
+# Test the classifiers
+nb_performance = test_classifier(nb_classifier, test_data)
+svm_performance = test_classifier(svm_classifier, test_data)
+
+# Show the results
+print ("\nNaive Bayes results")
+show_results(nb_performance)
+print ("\nSVM results")
+show_results(svm_performance)
+print("\n")
