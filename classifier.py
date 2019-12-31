cf912ef86420424258675e4f02cbaca8af3860df
adavideo <adavideo@gmail.com>
2019-12-31 13:00:52 +0100

    Now you can config if we use a glossary or not on the config file.

--- classifier.py
+++ classifier.py
@@ -7,6 +7,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.datasets import load_files
 import numpy as np
 
+
+# LOADING DATA FROM EXTERNAL FILES
+
 def config_path(parameter):
     config_file = open("config.txt")
     config_data = config_file.read().split("\n")
@@ -31,12 +34,29 @@ def load_glossary():
     glossary_file.close
     return glossary
 
-def build_vectorizer(bigram):
-    glossary = load_glossary()
-    if bigram:
-        vectorizer = CountVectorizer(vocabulary = glossary, ngram_range=(1, 2))
-    else:
-        vectorizer = CountVectorizer(vocabulary = glossary)
+def config_glossary():
+    config_file = open("config.txt")
+    config_data = config_file.read().split("\n")
+    with_glossary = (config_data[3] == "True")
+    print("With glossary: " + config_data[3])
+    return with_glossary
+
+
+# BUILDS AND TRAIN THE CLASSIFIERS
+
+# Builds a CountVectorizer with the parameters especified
+def build_vectorizer(bigram, with_glossary):
+    if with_glossary:
+        glossary = load_glossary()
+        if bigram:
+            vectorizer = CountVectorizer(vocabulary = glossary, ngram_range=(1, 2))
+        else: # unigram
+            vectorizer = CountVectorizer(vocabulary = glossary)
+    else: # Without glossary
+        if bigram:
+            vectorizer = CountVectorizer(ngram_range=(1, 2))
+        else: # unigram
+            vectorizer = CountVectorizer()
     return vectorizer
 
 # Builds the naive bayes classifier
@@ -54,35 +74,55 @@ def svm_classifier(vectorizer):
                                 penalty='l2',alpha=1e-3, max_iter=20, random_state=42))])
     return classifier
 
-# Builds the type of classifier especified by the parameters
-def build_classifier(classifier_type, bigram):
-    if bigram:
-        print("Building bigram " + classifier_type + " classifier")
+def get_classifier_name(classifier_type, bigram, with_glossary):
+    name = ""
+    if not with_glossary:
+        if bigram:
+            name += "bigram "
+        else:
+            name += "unigram "
+    name += classifier_type
+    if with_glossary:
+        name += " with glossary"
     else:
-        print("Building unigram " + classifier_type + " classifier")
-    vectorizer = build_vectorizer(bigram)
+        name += " without glossary"
+    return name
+
+# Builds the type of classifier especified by the parameters
+def build_classifier(classifier_type, with_glossary, bigram):
+    classifier_name = get_classifier_name(classifier_type, bigram, with_glossary)
+    print("Building classifier: " + classifier_name)
+    vectorizer = build_vectorizer(bigram, with_glossary)
     if classifier_type == "Naive Bayes":
         classifier = naive_bayes_classifier(vectorizer)
     elif classifier_type == "SVM":
         classifier = svm_classifier(vectorizer)
     return classifier
 
-def build_and_train_classifier(classifier_type, training_data, bigram):
-    classifier = build_classifier(classifier_type, bigram)
+def build_and_train_classifier(classifier_type, training_data, with_glossary, bigram):
+    classifier = build_classifier(classifier_type, with_glossary, bigram)
     # Trains the classifier with the training data
     classifier.fit(training_data.data, training_data.target)
     return classifier
 
-def build_and_train_all_classifiers(training_data):
+def build_and_train_all_classifiers(training_data, with_glossary):
     classifiers = {"Naive Bayes": [], "SVM": [] }
-    for classifier_type in classifiers:
-        # Building and training unigram classifier
-        unigram_classifier = build_and_train_classifier(classifier_type, training_data, bigram = False)
-        classifiers[classifier_type].append(unigram_classifier)
-        # Building and training bigram classifier
-        bigram_classifier = build_and_train_classifier(classifier_type, training_data, bigram = True)
-        classifiers[classifier_type].append(bigram_classifier)
+    if with_glossary:
+        for classifier_type in classifiers:
+            classifier = build_and_train_classifier(classifier_type, training_data, with_glossary, bigram = False)
+            classifiers[classifier_type].append(classifier)
+    else:
+        for classifier_type in classifiers:
+            # Building and training unigram classifier
+            unigram_classifier = build_and_train_classifier(classifier_type, training_data, with_glossary, bigram = False)
+            classifiers[classifier_type].append(unigram_classifier)
+            # Building and training bigram classifier
+            bigram_classifier = build_and_train_classifier(classifier_type, training_data, with_glossary, bigram = True)
+            classifiers[classifier_type].append(bigram_classifier)
     return classifiers
+
+
+# CALCULATE RESULTS
 
 # Compare the predictions with the right categories and returns
 # if the prediction is correct or not, ordered by category
@@ -112,11 +152,12 @@ def test_classifier(classifier, test_data):
     performance["by_category"] = calculate_performance_by_category(predictions, test_data.target)
     return performance
 
-def show_results_head(classifier_type, bigram):
-    if bigram:
-        print("\nBigram " + classifier_type + " results")
-    else:
-        print("\nUnigram " + classifier_type+ " results")
+
+# SHOW RESULTS
+
+def show_results_head(classifier_type, bigram, with_glossary):
+    classifier_name = get_classifier_name(classifier_type, bigram, with_glossary)
+    print("\nResults for " + classifier_name + " classifier")
 
 def show_results(performance, category_names):
     mean = str(performance["mean"])
@@ -125,19 +166,25 @@ def show_results(performance, category_names):
         print(category_names[category])
         print(performance["by_category"][category])
 
+
+# MAIN FUNCTION
+
 # Load the datasets
 training_data = load_dataset("train")
 test_data = load_dataset("test")
 category_names = test_data.target_names
 
+# Reads from the config file if we use a glossary or not
+with_glossary = config_glossary()
+
 # Build and train the classifiers
-classifiers = build_and_train_all_classifiers(training_data)
+classifiers = build_and_train_all_classifiers(training_data, with_glossary)
 
 # Test the classifiers and show the results
 for classifier_type in classifiers:
     counter = 0
     for classifier in classifiers[classifier_type]:
         performance = test_classifier(classifier, test_data)
-        show_results_head(classifier_type, counter==1)
+        show_results_head(classifier_type, counter==1, with_glossary)
         show_results(performance, category_names)
         counter += 1
